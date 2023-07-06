1a4b5f0235f9d20e1b080f772645b3b9709e72dd
Neset Can Salik <neset.salik@gmail.com>
2023-07-06 18:33:28 +0300

    Tasarım için giriş yapıldı

---
+++ flutter_tasarim_giris/android/app/src/main/kotlin/com/example/fluttertasarimgiris/flutter_tasarim_giris/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttertasarimgiris.flutter_tasarim_giris
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ row_column_stack_calismasi/android/app/src/main/kotlin/com/example/rowcolumnstackcalismasi/row_column_stack_calismasi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rowcolumnstackcalismasi.row_column_stack_calismasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
