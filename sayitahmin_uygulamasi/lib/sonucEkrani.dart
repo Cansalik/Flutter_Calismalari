e3514ab7da97eadeda2e9305130bc820e88e23a0
Neset Can Salik <neset.salik@gmail.com>
2023-08-03 12:36:29 +0300

    Sayı Tahmin Uygulaması Yapıldı.

---
+++ sayitahmin_uygulamasi/lib/sonucEkrani.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class sonucEkrani extends StatefulWidget {
+
+  late bool sonuc;
+
+  sonucEkrani({ required this.sonuc});
+
+  @override
+  State<sonucEkrani> createState() => _sonucEkraniState();
+}
+
+class _sonucEkraniState extends State<sonucEkrani> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Sonuç Ekranı"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            widget.sonuc ? Image.asset("resimler/mutlu_resim.png") : Image.asset("resimler/uzgun_resim.png"),
+            Text( widget.sonuc ? "Kazandınız" : "Kaybettiniz", style: TextStyle(color: Colors.black54,fontSize: 36),),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(
+                child: Text("TEKRAR OYNA", style: TextStyle(color: Colors.white),),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.purpleAccent
+                ),
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ sayitahmin_uygulamasi/lib/tahminEkrani.dart
@@ -0,0 +1,107 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sayitahmin_uygulamasi/sonucEkrani.dart';
+
+class tahminEkrani extends StatefulWidget {
+  const tahminEkrani({Key? key}) : super(key: key);
+
+  @override
+  State<tahminEkrani> createState() => _tahminEkraniState();
+}
+
+class _tahminEkraniState extends State<tahminEkrani> {
+
+  var tfTahmin = TextEditingController();
+  late int rasgeleSayi = 0;
+  late int kalanHak = 5;
+  late String ipucu = "";
+
+
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    rasgeleSayi = Random().nextInt(101); // 0 - 100 Arası Sayı üretir.
+    print("Rasgele Sayımız: $rasgeleSayi");
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Tahim Ekranı"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text("Kalan Hak: $kalanHak", style: TextStyle(color: Colors.lightBlue,fontSize: 30),),
+            Text("İpucu: $ipucu", style: TextStyle(color: Colors.black54,fontSize: 20),),
+            Padding(
+              padding: EdgeInsets.only(left: 25.0,right: 25.0),
+              child: TextField(
+                controller: tfTahmin,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  labelText: "Tahmin",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(
+                child: Text("TAHMİN ET", style: TextStyle(color: Colors.white),),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.purpleAccent
+                ),
+                onPressed: (){
+
+                  setState(() {
+                    kalanHak = kalanHak - 1;
+                  });
+
+                  late int tahmin = int.parse(tfTahmin.text);
+
+                  if(tahmin == rasgeleSayi)
+                  {
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => sonucEkrani(sonuc: true,)));
+                    return;
+                  }
+                  if(tahmin > rasgeleSayi)
+                  {
+                    setState(() {
+                      ipucu = "Tahminini Azalt";
+                    });
+                  }
+                  if(tahmin < rasgeleSayi)
+                  {
+                    setState(() {
+                      ipucu = "Tahminini Arttır";
+                    });
+                  }
+                  if(kalanHak == 0)
+                  {
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => sonucEkrani(sonuc: false,)));
+                    return;
+                  }
+                  tfTahmin.text = "";
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
