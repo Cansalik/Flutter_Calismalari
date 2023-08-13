574a296aeed6a5f867c8fd7251e6d35581766477
Neset Can Salik <neset.salik@gmail.com>
2023-08-13 23:09:42 +0300

    Filmler Uygulamasında hatalar düzeltildi.

--- filmler_uygulamasi/lib/FilmlerDAO.dart
+++ filmler_uygulamasi/lib/FilmlerDAO.dart
@@ -3,25 +3,26 @@ import 'package:filmler_uygulamasi/Kategoriler.dart';
 import 'package:filmler_uygulamasi/VeritabaniYardimcisi.dart';
 import 'package:filmler_uygulamasi/Yonetmenler.dart';
 
-class FilmlerDAO
-{
-  Future<List<Filmler>> filmGetir(int kategori_id) async
-  {
+class FilmlerDAO {
+
+  Future<List<Filmler>> tumFilmlerByKategoriId(int kategori_id) async {
+
     var db = await VeritabaniYardimcisi.veritabaniErisim();
 
-    List<Map<String,dynamic>> maps = await db.rawQuery("SELECT * FROM filmler,kategoriler,yonetmenler WHERE"
-        " filmler.kategori_id = kategoriler.kategori_id AND filmler.yonetmen_id = yonetmenler.yonetmen_id"
-        " AND filmler.kategori_id = $kategori_id");
+    List<Map<String,dynamic>> maps = await db.rawQuery("SELECT * FROM filmler,kategoriler,yonetmenler "
+        "WHERE filmler.kategori_id = kategoriler.kategori_id and  filmler.yonetmen_id = yonetmenler.yonetmen_id "
+        "and filmler.kategori_id = $kategori_id");
 
-    return List.generate(maps.length, (i)
-    {
+    return List.generate(maps.length, (i) {
       var satir = maps[i];
-      
-      var K = Kategoriler(satir["kategori_id"], satir["kategori_ad"]);
-      var Y = Yonetmenler(satir["yonetmen_id"], satir["yonetman_ad"]);
-      var F = Filmler(satir["film_id"], satir["film_ad"], satir["film_yil"], satir["film_resim"], K, Y);
 
-      return F;
+      var k = Kategoriler(satir["kategori_id"], satir["kategori_ad"]);
+      var y = Yonetmenler(satir["yonetmen_id"], satir["yonetmen_ad"]);
+      var f = Filmler(satir["film_id"], satir["film_ad"], satir["film_yil"], satir["film_resim"], k, y);
+
+      return f;
     });
   }
+
+
 }
